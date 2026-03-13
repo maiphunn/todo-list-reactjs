0625e03f5d17cff9bc74d5e5cbc15a8c5e7f9343
Nguyen Mai Phuong <maiphuong.nguyen.it@gmail.com>
2026-03-13 23:41:27 +0700

    connect MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import taskRoute from './routes/tasksRoutes.js';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+connectDB()
+
 app.use("/api/data", taskRoute)
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
